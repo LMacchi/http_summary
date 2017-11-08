93eca7d337891ef155c54faccf973c17d028b68c
LMacchi <lau.macchi@gmail.com>
2017-11-08 16:32:50 -0600

    Add http test

--- spec/unit/reports/http_summary_spec.rb
+++ spec/unit/reports/http_summary_spec.rb
@@ -8,22 +8,26 @@ describe http_summary do
     processor = Puppet::Transaction::Report.new('apply')
     processor.extend(http_summary)
   end
+  
+  before :each do
+    http.expects(:post).returns(httpok)
+    File.stubs(:exist?).with('/dev/null/http_summary.yaml').returns(true)
+  end
 
-  describe "when setting up the connection" do
-    let(:http) { stub_everything "http" }
-    let(:httpok) { Net::HTTPOK.new('1.1', 200, '') }
+  let(:http) { stub_everything "http" }
+  let(:httpok) { Net::HTTPOK.new('1.1', 200, '') }
+
+  describe "when setting up the connection with https" do
 
     before do
-      File.stubs(:exist?).with('/dev/null/http_summary.yaml').returns(true)
       YAML.stubs(:load_file).with('/dev/null/http_summary.yaml').returns(
         {
           'url' => 'https://localhost:8888',
         }
       )
-      http.expects(:post).returns(httpok)
     end
 
-    it "configures the connection for ssl when using https" do
+    it "should send report via https" do
       Puppet::Network::HttpPool.expects(:http_instance).with(
         'localhost', 8888, true
       ).returns http
@@ -31,6 +35,27 @@ describe http_summary do
       processor.add_metric(:resources, {"total" => 178, "failed" => 0, "changed" => 0})
     
       processor.process
+    end
+
+    describe "when setting up the connection with http" do
+
+      before do
+        YAML.stubs(:load_file).with('/dev/null/http_summary.yaml').returns(
+          {
+            'url' => 'http://localhost:8888',
+          }
+        )
+      end
+
+      it "should send report via http" do
+        Puppet::Network::HttpPool.expects(:http_instance).with(
+          'localhost', 8888, false
+        ).returns http
+
+        processor.add_metric(:resources, {"total" => 178, "failed" => 0, "changed" => 0})
+      
+        processor.process
+      end
     end
   end
 end
