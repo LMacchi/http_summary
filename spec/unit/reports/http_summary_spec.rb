83afcba4668fcd501b689fc14e8df5da8b8192c6
LMacchi <lau.macchi@gmail.com>
2017-11-08 17:31:52 -0600

    Add parser unit test

--- spec/unit/reports/http_summary_spec.rb
+++ spec/unit/reports/http_summary_spec.rb
@@ -8,14 +8,13 @@ describe http_summary do
     processor = Puppet::Transaction::Report.new('apply')
     processor.extend(http_summary)
   end
-  
-  before :each do
-    http.expects(:post).returns(httpok)
-    File.stubs(:exist?).with('/dev/null/http_summary.yaml').returns(true)
-  end
 
   let(:http) { stub_everything "http" }
   let(:httpok) { Net::HTTPOK.new('1.1', 200, '') }
+  
+  before :each do
+    File.stubs(:exist?).with('/dev/null/http_summary.yaml').returns(true)
+  end
 
   describe "when setting up the connection with https" do
 
@@ -25,37 +24,55 @@ describe http_summary do
           'url' => 'https://localhost:8888',
         }
       )
+      http.expects(:post).returns(httpok)
     end
+
 
     it "should send report via https" do
       Puppet::Network::HttpPool.expects(:http_instance).with(
         'localhost', 8888, true
       ).returns http
 
+      processor.add_metric(:resources, {"total" => 178, "failed" => 0, "changed" => 1})
+    
+      processor.process
+    end
+  end
+
+  describe "when setting up the connection with http" do
+
+    before do
+      YAML.stubs(:load_file).with('/dev/null/http_summary.yaml').returns(
+        {
+          'url' => 'http://localhost:8888',
+        }
+      )
+      http.expects(:post).returns(httpok)
+    end
+
+    it "should send report via http" do
+      Puppet::Network::HttpPool.expects(:http_instance).with(
+        'localhost', 8888, false
+      ).returns http
+
       processor.add_metric(:resources, {"total" => 178, "failed" => 0, "changed" => 0})
     
       processor.process
     end
+  end
 
-    describe "when setting up the connection with http" do
+  describe "when parsing reports" do
 
-      before do
-        YAML.stubs(:load_file).with('/dev/null/http_summary.yaml').returns(
-          {
-            'url' => 'http://localhost:8888',
-          }
-        )
-      end
+    it "should return the right json object" do
+      processor.expects(:host).returns('laura.puppetlabs.vm')
+      processor.expects(:time).returns('2017-11-06T21:44:40.258493862Z')
+      processor.expects(:status).returns('changed')
+      processor.add_metric(:resources, {"total" => 178, "failed" => 0, "changed" => 1})
 
-      it "should send report via http" do
-        Puppet::Network::HttpPool.expects(:http_instance).with(
-          'localhost', 8888, false
-        ).returns http
+      response = {"host":"laura.puppetlabs.vm","time":"2017-11-06T21:44:40.258493862Z","status":"changed","totalResources":178,"changedResources":1,"failedResources":0}
 
-        processor.add_metric(:resources, {"total" => 178, "failed" => 0, "changed" => 0})
-      
-        processor.process
-      end
+      expect(processor.parse_report(processor)).to eq(response.to_json)
     end
   end
+
 end
