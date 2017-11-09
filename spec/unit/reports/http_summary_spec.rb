70a1a7271634db6c7c87e44fa83bbe5b581eeb3d
LMacchi <lau.macchi@gmail.com>
2017-11-09 14:07:08 -0600

    Non working tests

--- spec/unit/reports/http_summary_spec.rb
+++ spec/unit/reports/http_summary_spec.rb
@@ -4,21 +4,19 @@ require 'puppet/reports'
 http_summary = Puppet::Reports.report(:http_summary)
 
 describe http_summary do
-  let(:processor) do
-    processor = Puppet::Transaction::Report.new('apply')
-    processor.extend(http_summary)
-  end
-
   let(:http) { stub_everything 'http' }
   let(:httpok) { Net::HTTPOK.new('1.1', 200, '') }
   let(:host) { 'laura.puppetlabs.vm' }
   let(:time) { '2017-11-06T21:44:40.258493862Z' }
   let(:resources) { { 'total' => 178, 'failed' => 0, 'changed' => 1 } }
+
   before :each do
     File.stubs(:exist?).with('/dev/null/http_summary.yaml').returns(true)
   end
 
-  describe 'when setting up the connection with https' do
+  describe 'when providing url with https in the conf file' do
+    let(:processor) { Puppet::Transaction::Report.new('apply').extend(http_summary) }
+
     before do
       YAML.stubs(:load_file).with('/dev/null/http_summary.yaml').returns(
         'url' => 'https://localhost:8888'
@@ -37,12 +35,15 @@ describe http_summary do
     end
   end
 
-  describe 'when setting up the connection with http' do
-    before do
+  describe 'when providing url with http in the conf file' do
+   let(:processor) { Puppet::Transaction::Report.new('apply').extend(http_summary) }
+
+   before do
       YAML.stubs(:load_file).with('/dev/null/http_summary.yaml').returns(
         'url' => 'http://localhost:8888'
       )
       http.expects(:post).returns(httpok)
+      processor.add_metric(:resources, resources)
     end
 
     it 'should send report via http' do
@@ -50,18 +51,47 @@ describe http_summary do
         'localhost', 8888, false
       ).returns http
 
-      processor.add_metric(:resources, resources)
-
       processor.process
     end
   end
 
+  describe 'when report has no changes' do
+    let(:processor) { Puppet::Transaction::Report.new('apply').extend(http_summary) }
+    before do
+      processor.finalize_report
+      processor.add_metric(:resources, resources)
+    end
+
+    it 'should not post to url' do
+      processor.expects(:status).returns('unchanged')
+      processor.expects(:send).never
+    end
+  end
+
+  describe 'when report has status undefined' do
+    let(:processor) { Puppet::Transaction::Report.new('apply').extend(http_summary) }
+    before do
+      processor.finalize_report
+      processor.add_metric(:resources, resources)
+    end
+
+    it 'should not post to url' do
+      processor.expects(:status).returns('undefined')
+      processor.expects(:send).never
+    end
+  end
+
   describe 'when parsing reports' do
-    it 'should return the right json object' do
+    let(:processor) { Puppet::Transaction::Report.new('apply').extend(http_summary) }
+    before do
+      processor.finalize_report
+      processor.add_metric(:resources, resources)
+    end
+
+   it 'should return the right json object' do
       processor.expects(:host).returns(host)
       processor.expects(:time).returns(time)
       processor.expects(:status).returns('changed')
-      processor.add_metric(:resources, resources)
 
       response = {
         'host'             => host,
